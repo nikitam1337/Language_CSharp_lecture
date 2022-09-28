0af223eac84f87ac45438ebc7c465c05e270dd80
nikitam1337 <nikitam.l@mail.ru>
2022-09-28 21:53:17 +0300

    random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int nubmerA = 31;
-int numberB = 5;
+﻿int nubmerA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(nubmerA);
+int numberB = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(numberB);
 int result = nubmerA + numberB;
 Console.WriteLine(result);
